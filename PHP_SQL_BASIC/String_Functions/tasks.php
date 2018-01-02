497301ffe77b075b0bba6fc7dd69a4189947254c
reddowell <vikt.anmail@gmail.com>
2018-01-02 19:07:50 +0300

    String Functions

--- PHP_SQL_BASIC/String_Functions/tasks.php
+++ PHP_SQL_BASIC/String_Functions/tasks.php
@@ -29,9 +29,9 @@ echo '</pre>';
 //      4. В переменной $date лежит дата в формате '31-12-2030'. Преобразуйте эту дату в формат '2030.12.31'.
 
 $string_task4 = '31-12-2030';
-$array_task4 = explode('-',$string_task4);
+$array_task4 = explode('-', $string_task4);
 $array_task4 = array_reverse($array_task4);
-$result_task4 = implode('.',$array_task4);
+$result_task4 = implode('.', $array_task4);
 echo '<pre>';
 var_dump($result_task4);
 echo '</pre>';
@@ -40,19 +40,19 @@ echo '</pre>';
 //      5. Дана строка 'london is the capital of great britain'. Сделайте из нее строку 'London Is The Capital Of Great Britain'.
 
 $string_task5 = 'london is the capital of great britain';
-echo "\n",ucwords($string_task5);
+echo "\n", ucwords($string_task5);
 
 
 //      6. Дана строка 'LONDON'. Сделайте из нее строку 'London'
 
 $string_task6 = 'LONDON';
-echo "<pre>\n</pre>",ucfirst(strtolower($string_task6));
+echo "<pre>\n</pre>", ucfirst(strtolower($string_task6));
 
 
 //      7. Дана строка 'html css php'. Найдите количество символов в этой строке.
 
 $string_task7 = 'html css php';
-echo "<pre>\n</pre>",strlen($string_task7);
+echo "<pre>\n</pre>", strlen($string_task7);
 
 
 //      8. Дана переменная $password, в которой хранится пароль пользователя. Если количество символов пароля
@@ -60,134 +60,131 @@ echo "<pre>\n</pre>",strlen($string_task7);
 // иначе сообщение о том, что нужно придумать другой пароль.
 
 $password = '5678932321';
-if(strlen($password)>5 && strlen($password)<10){
-    echo "<pre>\n</pre>","OK";
-} else{
-    echo "<pre>\n</pre>","NO";
+if (strlen($password) > 5 && strlen($password) < 10) {
+    echo "<pre>\n</pre>", "OK";
+} else {
+    echo "<pre>\n</pre>", "NO";
 }
 
 
 //      9. Дана строка 'html css php'. Вырежьте из нее и выведите на экран слово 'html', слово 'css' и слово 'php'
 $string_task9 = 'html css php';
-$result_task9 = explode(' ',$string_task9);
+$result_task9 = explode(' ', $string_task9);
 echo '<pre>';
 var_dump($result_task9);
 echo '</pre>';
 
 //      10. Дана строка. Вырежите и выведите на экран последние 3 символа этой строки
 $string_task10 = 'my string';
-echo "<pre>\n</pre>",substr($string_task10,-3);
+echo "<pre>\n</pre>", substr($string_task10, -3);
 
 
 //      11. Дана строка. Проверьте, что она начинается на 'http://'. Если это так, выведите 'да', если не так - 'нет'a
 $string_task11 = 'http://mysiite,com';
-if(substr($string_task11,0,7)=='http://'){
-    echo "<pre>\n</pre>",'HTTP';
+if (substr($string_task11, 0, 7) == 'http://') {
+    echo "<pre>\n</pre>", 'HTTP';
 } else {
-    echo "<pre>\n</pre>",'NOT HTTP';
+    echo "<pre>\n</pre>", 'NOT HTTP';
 }
 
 
 //      12. Дана строка. Если в этой строке более 5-ти символов - вырежите из нее первые 5 символов, добавьте
 //троеточие в конец и выведите на экран. Если же в этой строке 5 и менее символов - просто выведите эту строку на экран
 $string_task12 = "my string task 12";
-if(strlen($string_task12) > 5){
-    echo "<pre>\n</pre>",substr($string_task12,0,5).'...';
-} else{
-    echo "<pre>\n</pre>","string lenght < 5";
+if (strlen($string_task12) > 5) {
+    echo "<pre>\n</pre>", substr($string_task12, 0, 5) . '...';
+} else {
+    echo "<pre>\n</pre>", "string lenght < 5";
 }
 
 
 //      13. Дана строка '31.12.2013'. Замените все точки на дефисы
 $string_task13 = '31.12.2013';
-echo "<pre>\n</pre>",str_replace('.','-',$string_task13);
+echo "<pre>\n</pre>", str_replace('.', '-', $string_task13);
 
 echo '<pre>';
 
 //      14. Дана строка $str. Замените в ней все буквы 'a' на цифру 1, буквы 'b' - на 2, а буквы 'c' - на 3
 $string_task14 = 'abcabc';
-echo "\n",str_replace(['a','b','c'],[1,2,3],$string_task14);
-
+echo "\n", str_replace(['a', 'b', 'c'], [1, 2, 3], $string_task14);
 
 
 //      15. Дана строка с буквами и цифрами, например, '1a2b3c4b5d6e7f8g9h0'. Удалите из нее все цифры. То есть в нашем
 //       случае должна получится строка 'abcbdefgh'.
 $string_task15 = '1a2b3c4b5d6e7f8g9h0';
-echo "\n",str_replace([1,2,3,4,5,6,7,8,9,0],'',$string_task15);
-
+echo "\n", str_replace([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], '', $string_task15);
 
 
 //      16. Дана строка $str. Замените в ней все буквы 'a' на цифру 1, буквы 'b' - на 2, а буквы 'c' - на 3.
 //      Решите задачу двумя способами работы с функцией strtr (массив замен и две строки замен).
 $string_task16 = 'abcabcqwe';
-echo "\n",strtr($string_task16,['a' => 1,'b' => 2,'c' => 3]);
-echo "\n",strtr($string_task16,'abc','123');
+echo "\n", strtr($string_task16, ['a' => 1, 'b' => 2, 'c' => 3]);
+echo "\n", strtr($string_task16, 'abc', '123');
 
 
 //      17. Дана строка $str. Вырежите из нее подстроку с 3-го символа (отсчет с нуля),
 //       5 штук и вместо нее вставьте '!!!'.
 $string_task17 = 'myStringTask16';
-echo "\n",substr_replace($string_task17,'!!!',2,5);
+echo "\n", substr_replace($string_task17, '!!!', 2, 5);
 
 
 //      18. Дана строка 'abc abc abc'. Определите позицию первой буквы 'b'.
 $string_task18 = 'abc abc abc';
-echo "\n",strpos($string_task18,'b'),'(+1) - first b';
+echo "\n", strpos($string_task18, 'b'), '(+1) - first b';
 
 //      19. Дана строка 'abc abc abc'. Определите позицию последней буквы 'b'
 $string_task19 = 'abc abc abc';
-echo "\n",strrpos($string_task19,'b'),'(+1) - last b';
+echo "\n", strrpos($string_task19, 'b'), '(+1) - last b';
 
 //      20. Дана строка 'abc abc abc'. Определите позицию первой найденной буквы 'b',
 //       если начать поиск не с начала строки, а с позиции 3
 $string_task20 = 'abc abc abc';
-echo "\n",strpos($string_task20,'b',3),'(+1) [3 - start]';
+echo "\n", strpos($string_task20, 'b', 3), '(+1) [3 - start]';
 
 //      21. Дана строка 'aaa aaa aaa aaa aaa'. Определите позицию второго пробела
 $string_task21 = 'aaa aaa aaa aaa aaa';
-echo "\n",strpos($string_task21,' ',strpos($string_task21,' ') + 1),'(+1) [2 - space]';
+echo "\n", strpos($string_task21, ' ', strpos($string_task21, ' ') + 1), '(+1) [2 - space]';
 
 //      22. Проверьте, что в строке есть две точки подряд. Если это так - выведите 'есть', если не так - 'нет'.
 $string_task22 = 'myString.. and ..';
-if(strpos($string_task22,'..')){
-    echo "\n",'есть';
-} else echo "\n",'нет';
+if (strpos($string_task22, '..')) {
+    echo "\n", 'есть';
+} else echo "\n", 'нет';
 
 //      23. Проверьте, что строка начинается на 'http://'. Если это так - выведите 'да', если не так - 'нет'.
 $string_task23 = 'http://mysite.com';
-if(strpos($string_task23,'http:/') == 0){
-    echo "\n",'да';
-} else echo "\n",'нет';
+if (strpos($string_task23, 'http:/') == 0) {
+    echo "\n", 'да';
+} else echo "\n", 'нет';
 
 
 //      24. Дана строка 'html css php'. С помощью функции explode запишите каждое слово этой строки
 //       в отдельный элемент массива
 $string_task24 = 'html css php';
-var_dump($array_task24 = explode(' ',$string_task24));
+var_dump($array_task24 = explode(' ', $string_task24));
 
 
 //      25. Дан массив с элементами 'html', 'css', 'php'. С помощью функции implode создайте строку из этих элементов,
 //       разделенных запятыми.
-$array_task25 = ['html','css','php'];
-var_dump($string_task25 = implode(',',$array_task25));
+$array_task25 = ['html', 'css', 'php'];
+var_dump($string_task25 = implode(',', $array_task25));
 
 //      26. В переменной $date лежит дата в формате '2013-12-31'. Преобразуйте эту дату в формат '31.12.2013'.
 $date_task26 = '2013-12-31';
-var_dump(implode('.',array_reverse(explode('-',$date_task26))));
-
+var_dump(implode('.', array_reverse(explode('-', $date_task26))));
 
 
 //      27. Дана строка '1234567890'. Разбейте ее на массив с элементами '12', '34', '56', '78', '90'.
 $string_task27 = '1234567890';
-var_dump(str_split($string_task27,2));
+var_dump(str_split($string_task27, 2));
 
 //      28. Дана строка '1234567890'. Разбейте ее на массив с элементами '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'.
 $string_task28 = '1234567890';
-var_dump(str_split($string_task28,1));
+var_dump(str_split($string_task28, 1));
 
 //      29. Дана строка '1234567890'. Сделайте из нее строку '12-34-56-78-90' не используя цикл.
 $string_task29 = '1234567890';
-var_dump($resultString29 = implode('-',str_split($string_task29,2)));
+var_dump($resultString29 = implode('-', str_split($string_task29, 2)));
 
 //      30. Дана строка. Очистите ее от концевых пробелов.
 $string_task30 = ' my string ';
@@ -195,14 +192,13 @@ var_dump(trim($string_task30));
 
 //      31. Дана строка '/php/'. Сделайте из нее строку 'php', удалив концевые слеши.
 $string_task31 = '/php/';
-var_dump(trim($string_task31,'/'));
+var_dump(trim($string_task31, '/'));
 
 //      32. Дана строка 'слова слова слова.'. В конце этой строки может быть точка, а может и не быть.
 //      Сделайте так, чтобы в конце этой строки гарантировано стояла точка. То есть: если этой точки нет - ее надо добавить,
 //      а если есть - ничего не делать. Задачу решите через rtrim без всяких ифов.
 $string_task32 = 'string string string.';
-var_dump(trim($string_task32,'.').'.');
-
+var_dump(trim($string_task32, '.') . '.');
 
 
 //      33. Дана строка '12345'. Сделайте из нее строку '54321'.
@@ -212,9 +208,9 @@ var_dump(strrev($string_task33));
 //      34. Проверьте, является ли слово палиндромом
 //       (одинаково читается во всех направлениях, примеры таких слов: madam, otto, kayak, nun, level).
 $string_task34 = 'nun';
-if($string_task34 == strrev($string_task34)){
-    echo 'Полиндром',"\n";
-} else echo 'Не Полиндром',"\n";
+if ($string_task34 == strrev($string_task34)) {
+    echo 'Полиндром', "\n";
+} else echo 'Не Полиндром', "\n";
 
 //      35. Дана строка. Перемешайте символы этой строки в случайном порядке.
 $string_task35 = 'My String';
@@ -223,7 +219,94 @@ var_dump(str_shuffle($string_task35));
 //      36. Создайте строку из 6-ти случайных маленьких латинских букв так, чтобы буквы не повторялись.
 //       Нужно сделать так, чтобы в нашей строке могла быть любая латинская буква, а не ограниченный набор.
 $allCharsTask36 = 'qwertyuiopasdfghjklzxcvbnm';
-var_dump(substr(str_shuffle($allCharsTask36),0,6));
+var_dump(substr(str_shuffle($allCharsTask36), 0, 6));
+
+
+//      37.  Дана строка '12345678'. Сделайте из нее строку '12 345 678'.
+$string_task37 = '12345678';
+var_dump(number_format($string_task37, 0, '', ' '));
+
+
+//      38. Нарисуйте пирамиду, как показано на рисунке, только у вашей пирамиды должно быть 9 рядов, а не 5.
+//       Решите задачу с помощью одного цикла и функции str_repeat.
+$string_task38 = 'x';
+for ($i = 0; $i < 9; $i++) {
+    echo str_repeat($string_task38, $i), "\n";
+}
+
+//      39. Нарисуйте пирамиду, как показано на рисунке. Решите задачу с помощью одного цикла и функции str_repeat.
+for ($i = 0; $i < 9; $i++) {
+    echo str_repeat($i, $i), "\n";
+}
+
+//      40. Дана строка 'html, <b>php</b>, js'. Удалите теги из этой строки.
+$string_task40 = 'html, <b>php</b>, js';
+echo strip_tags($string_task40), "\n";
+
+//      41. Дана строка $str. Удалите все теги из этой строки, кроме тегов <b> и <i>
+$string_task41 = 'my <b>string</b> <em>another</em> <i>my</i> string';
+echo strip_tags($string_task41, '<b><i>'), "\n";
+
+
+//      42. Дана строка 'html, <b>php</b>, js'. Выведите ее на экран 'как есть':
+//       то есть браузер не должен преобразовать <b> в жирный.
+$string_task42 = 'html, <b>php</b>, js';
+echo htmlspecialchars($string_task42), "\n";
+
+
+//      43.Узнайте код символов 'a', 'b', 'c', пробела
+echo ord('a'), ord('b'), ord('c'), ord(' '), "\n";
+
+
+//      44. Выведите на экран символ с кодом 33
+echo chr(33), "\n";
+
+//      45. Запишите в переменную $str случайный символ - большую букву латинского алфавита.
+$string_task45 = chr(mt_rand(65, 90));
+
+
+//      46. Запишите в переменную $str случайную строку $len длиной, состоящую из маленьких букв латинского алфавита.
+$len = rand(1, 10);
+$string_task46 = '';
+for ($i = 0; $i < $len; $i++) {
+    $string_task46 .= chr(mt_rand(97, 122));
+}
+echo $string_task46, "\n";
+
+
+//      47. Дана буква английского алфавита. Узнайте, она маленькая или большая
+$char_task47 = 'A';
+if (ord($char_task47) >= 97 && ord($char_task47) <= 122) {
+    echo "Small", "\n";
+} else if (ord($char_task47) >= 65 && ord($char_task47) <= 90) {
+    echo "Big", "\n";
+} else echo "Not a letter", "\n";
+
+//      48. Дана строка 'ab-cd-ef'. С помощью функции strchr выведите на экран строку '-cd-ef'
+$string_task48 = 'ab-cd-ed';
+echo strchr($string_task48, '-'), "\n";
+
+//      49. Дана строка 'ab-cd-ef'. С помощью функции strrchr выведите на экран строку '-ef'.
+$string_task49 = 'ab-cd-ef';
+echo strrchr($string_task49, '-'), "\n";
+
+
+//      50. Дана строка 'ab--cd--ef'. С помощью функции strstr выведите на экран строку '--cd--ef'
+$string_task50 = 'ab--cd--ef';
+echo strstr($string_task50, '--'), "\n";
+
+//      51. Преобразуйте строку 'var_test_text' в 'varTestText'.
+//       Скрипт, конечно же, должен работать с любыми аналогичными строками
+$string_task51 = 'var_test_text';
+echo lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $string_task51)))), "\n";
+
+
+//      52. Дана строка следующего вида: 5 цифр, затем пробел, затем еще 5 цифр.
+//      Например, дана такая строка '12345 67890'. Сделайте из нее строку '54321 09876'
+$string_task52 = '12345 67890';
+$arr_task52 = explode(' ',$string_task52);
+foreach ($arr_task52 as $key=>$element){
+    $arr_task52[$key] = strrev($element);
+}
+echo implode(' ',$arr_task52),"\n";
 echo '</pre>';
-
-
